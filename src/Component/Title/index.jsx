5c34ad912753fab517455b41a4a22bad697ad1fa
Weldon <weldonthallysson.dev@gmail.com>
2023-06-02 20:23:34 -0300

    Componente title criado, primeiro componente da pagina

---
+++ src/Component/Title/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css'
+
+const Title = () => {
+    return (
+        <div className="Container__Title">
+            <div className="Traco" />
+
+            <div className="Conteiner__Title" >
+                <h1 className="Title">Oportunidade de Terceiros</h1>
+            </div>
+        </div>
+    )
+}
+
+
+export default Title;
